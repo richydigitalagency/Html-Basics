390a28891ca62c39f92ef91996c9cf0377958532
richydigitalagency <richydigitalagency@gmail.com>
2025-02-22 21:03:12 +0100

    I updated the assignment file

--- assignment.js
+++ assignment.js
@@ -43,3 +43,70 @@ while (i <= 10) {
     console.log(i);
     i++;
 }
+
+// Sum of numbers in Array
+
+// ** for loop**
+function sumForloop(numbers) {
+    let sum = o;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+    return sum;
+}
+
+// ** while loop**
+function sumWhileloop(numbers) {
+    let sum = 0;
+    let i = 0;
+    while (i < numbers.length) {
+        sum += numbers[i];
+        i++;
+    }
+    return sum;
+}
+
+// printing Elements of an Array
+// ** for loop**
+function printForloop(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+// ** while loop**
+function printWhileloop(arr) {
+    let i = 0;
+    while (i < arr.length) {
+        console.log(arr[i]);
+        i++;
+    }
+}
+
+// Even or Odd Number
+
+function isEven(num) {
+    if (num % 2 === 0) {
+    return `${num} is even`;
+} else {
+    return `${num} is odd`;
+   }
+}
+
+// Grade Calculator
+
+function getGrade(score) {
+    if (score >= 90) {
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    }  else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else if (score >= 40) {
+        return "E";
+    } else {
+        return "F";
+    }
+}
